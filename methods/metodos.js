b173346070a92ed9e87986fcbe8a348c5fbce637
slaiton <jstivenlaiton@gmail.com>
2022-11-10 11:01:01 -0500

    Inicio
    Se ajusta Vista Index, y archivos oficiales

---
+++ methods/metodos.js
@@ -0,0 +1,23 @@
+var filas = 0;
+    columnas = 0;
+    matriz = [[]]
+    coheficiente = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+function laPlace(matriz)
+{
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+}
