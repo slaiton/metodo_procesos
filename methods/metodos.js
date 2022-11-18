2dbc43185ea87fb239cc347a3891846cae1c5e96
slaiton <jstivenlaiton@gmail.com>
2022-11-18 16:31:27 -0500

    ok
     Proyecto  OK

--- methods/metodos.js
+++ methods/metodos.js
@@ -1,44 +1,52 @@
-// $(document).ready(function(){
-//       $(".metodo").on('click', function(){
-//       var metodo = $(this).attr('id');
-
-//       switch (metodo) {
-//         case 'laplace':
-//             alert(metodo)
-//             matriz = [[4,5,6,5],[10,15,0,3],[11,8,1,5],[6,4,2,9]];
-//             resp =  laPlace(matriz);
-//             console.log(resp);
-//             break;
-//             default:
-//                 break;
-//             }
-//         });
-//     });
 
 
-var filas = 0,
-    columnas = 0,
-    coheficiente = 0.65;
-/*matriz = [
-    [4,5,6,5],
-    [10,15,0,-3],
-    [11,8,1,5],
-    [6,4,2,9]
-];
+    // $(document).ready(function(){
+    //       $(".metodo").on('click', function(){
+    //       var metodo = $(this).attr('id');
+  
+    //       switch (metodo) {
+    //         case 'laplace':
+                
+    //             alert(metodo)
+                
+    //             matriz = [[4,5,6,5],[10,15,0,3],[11,8,1,5],[6,4,2,9]];
+    //             resp =  laPlace(matriz);
+                
+    //             console.log(resp);
+                
+                
+    //             break;
+                
+    //             default:
+    //                 break;
+    //             }
+                
+    //         });
+    //     });
 
 
-console.log("Optimista", optimista(matriz));
-console.log("Pesimista", pesimista(matriz));
-console.log("Hurwicz", hurwicz(matriz, coheficiente));
-console.log("Savage", savage(matriz));
-*/
-// resp =  laPlace(matriz);            
-// console.log(resp);
 
 
-function laPlace(matriz) {
-    console.log("matriz:");
-    console.log(matriz);
+
+
+
+
+
+var filas = 0;
+    columnas = 0;
+    // matriz = [[4,5,6,5],[10,15,0,3],[11,8,1,5],[6,4,2,9]];
+    coheficiente = 0;
+
+    // resp =  laPlace(matriz);            
+    // console.log(resp);
+                
+
+    function laPlace(matriz)
+    {
+
+        console.log("matriz:");
+        console.log(matriz);
+
 
     var coh = matriz.length;
     var totalFila = 0;
@@ -46,7 +54,7 @@ function laPlace(matriz) {
     var mayor = 0;
     var anterior = 0;
 
-    for (let a = 0; a < matriz.length; a++) {
+      for (let a = 0; a < matriz.length; a++) {
         var linea = matriz[a];
         // console.log("FILA #" + a);
         totalFila = 0;
@@ -56,104 +64,109 @@ function laPlace(matriz) {
             totalFila += element;
         }
 
-        totalFinal = totalFila / coh;
-
-        if (totalFinal > anterior) {
+        totalFinal = totalFila/coh;
+        
+        if(totalFinal > anterior)
+        {
             mayor = totalFinal;
-            //   console.log(totalFinal + "Mayor que "+anterior);
+        //   console.log(totalFinal + "Mayor que "+anterior);
         }
         anterior = totalFinal;
     }
-    return mayor;
-}
 
+      return mayor;
 
-function obtenerMenor( arr ){
-    var menor = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-        if( arr[i] < menor ){
-            menor = arr[i];
+    }
+
+    function obtenerMenor( arr ){
+        var menor = arr[0];
+        for (let i = 0; i < arr.length; i++) {
+            if( arr[i] < menor ){
+                menor = arr[i];
+            }
         }
+        return menor;
     }
-    return menor;
-}
-
-function obtenerMayor( arr ){
-    var mayor = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-        if( arr[i] > mayor ){
-            mayor = arr[i];
+    
+    function obtenerMayor( arr ){
+        var mayor = arr[0];
+        for (let i = 0; i < arr.length; i++) {
+            if( arr[i] > mayor ){
+                mayor = arr[i];
+            }
         }
+        return mayor;
     }
-    return mayor;
-}
-
-
-function pesimista( matriz ){
-    var menor = [];
-    for (let i = 0; i < matriz.length; i++) {
-        menor[i] = obtenerMenor( matriz[i] );
-        console.log("El menor es: ", menor[i]);
+    
+    
+    function pesimista( matriz ){
+        var menor = [];
+        for (let i = 0; i < matriz.length; i++) {
+            menor[i] = obtenerMenor( matriz[i] );
+            console.log("El menor es: ", menor[i]);
+        }
+    
+        var mayor = obtenerMayor( menor); menor[0];
+        console.log("El mayor es: ", mayor);
+    
+        return mayor;
     }
-
-    var mayor = obtenerMayor( menor); menor[0];
-    console.log("El mayor es: ", mayor);
-
-    return mayor;
-}
-
-function optimista( matriz ){
-    var max = [];
-    for (let i = 0; i < matriz.length; i++) {
-        max[i] = obtenerMayor( matriz[i] );
-        console.log("El mayor es: ", max[i]);
+    
+    function optimista( matriz ){
+        var max = [];
+        for (let i = 0; i < matriz.length; i++) {
+            max[i] = obtenerMayor( matriz[i] );
+            console.log("El mayor es: ", max[i]);
+        }
+    
+        var mayor = obtenerMayor( max );
+        console.log("El mayor es: ", mayor);
+    
+        return mayor;
     }
-
-    var mayor = obtenerMayor( max );
-    console.log("El mayor es: ", mayor);
-
-    return mayor;
-}
-
-function hurwicz( matriz, coheficiente ){
-    var valor = [];
-    for (let i = 0; i < matriz.length; i++) {
-        var menor = obtenerMenor( matriz[i] );
-        var mayor = obtenerMayor( matriz[i] );
-        valor[i] =  Math.round((mayor * coheficiente) + (menor * ( 1 - coheficiente)) * 100) / 100;
-        console.log( "Menor ", menor, " Mayor ", mayor, " Ve ", valor[i]);
+    
+    function hurwicz( matriz, coheficiente ){
+        console.log(matriz);
+        console.log(coheficiente);
+        var valor = [];
+        for (let i = 0; i < matriz.length; i++) {
+            var menor = obtenerMenor( matriz[i] );
+            var mayor = obtenerMayor( matriz[i] );
+            valor[i] =  (mayor * coheficiente) + (menor * ( 1 - coheficiente));
+            console.log( "Menor ", menor, " Mayor ", mayor, " Ve ", valor[i]);
+            
+        }
+        return obtenerMayor( valor );
+    }
+    
+    function obtenerColumna(matriz, columna){
+        var column = [];
+        for(var i=0; i<matriz.length; i++){
+           column.push(matriz[i][columna]);
+        }
+        return column;
+     }
+    
+    function savage( matriz ){
+        var mayor = [];
+        var valorMatriz = matriz;
+        var valorEsperado = [];
+        for (let i = 0; i < matriz.length; i++) {
+            mayor[i] = obtenerMayor( obtenerColumna( matriz, i ) );
+            console.log(mayor[i])
+        }
         
-    }
-    return obtenerMayor( valor );
-}
-
-function obtenerColumna(matriz, columna){
-    var column = [];
-    for(var i=0; i<matriz.length; i++){
-       column.push(matriz[i][columna]);
-    }
-    return column;
- }
-
-function savage( matriz ){
-    var mayor = [];
-    var valorMatriz = matriz;
-    var valorEsperado = [];
-    for (let i = 0; i < matriz.length; i++) {
-        mayor[i] = obtenerMayor( obtenerColumna( matriz, i ) );
-    }
-    
-    for (let i = 0; i < matriz.length; i++) {
-        for (let j = 0; j < matriz[i].length; j++) {
-            valorMatriz[i][j] = mayor[i] - matriz[i][j];
+        for (let i = 0; i < matriz.length; i++) {
+            for (let j = 0; j < matriz[i].length; j++) {
+                valorMatriz[i][j] = mayor[j] - matriz[i][j];
+            }
         }
-    }
+        
+        for (let i = 0; i < matriz.length; i++) {
+            valorEsperado[i] = obtenerMayor( matriz[i] );
+        }
     
-    for (let i = 0; i < matriz.length; i++) {
-        valorEsperado[i] = obtenerMayor( matriz[i] );
+        console.log( valorMatriz );
+        console.log( valorEsperado );
+        return obtenerMenor(valorEsperado);
     }
-
-    console.log( valorMatriz );
-    console.log( valorEsperado );
-    return obtenerMenor(valorEsperado);
-}
--- views/main.js
+++ views/main.js
@@ -1,87 +1,116 @@
-$(document).ready(function () {
-    $(".valorEsp").on('submit', function (e) {
-        e.preventDefault();
+$(document).ready(function(){
 
-        var method = $("#method").val();
+
+
+$(".valorEsp").on('submit', function(e){
+    e.preventDefault();
+
+    var method = $("#method").val();
+
+    console.log(method);
+
+    if (method == 0) {
+
+        $("#method").focus();
+
+    }else{
+        var matriz = getMatriz();
+        switch (method[0]) {
+            case 'laplace':
+                var result = laPlace(matriz); 
+                break;
+
+            case 'hurwicz':
+                var result = hurwicz(matriz, 0.65);
+                break;
+
+            case 'savage':
+                var result = savage(matriz);
+                break;
+                    
+            case 'optimista':
+                var result = optimista(matriz);
+                break;
+
+            case 'pesimista':
+                var result = pesimista(matriz);
+                break;
         
-        if (method == 0) {
-            $("#method").focus();
-        } else {
-            method = method[0];
-            var matriz = getMatriz();
-            var coheficiente = 0.65;
-            var result = "";
-console.log( "El metodo es: ", method);
-            switch (method) {
-                case 'laplace': 
-                    console.log("Inicia laplace");
-                    result = laPlace(matriz) ;
+        
+            default:
                 break;
-                case 'hurwicz': 
-                    console.log("Inicia hurwicz");
-                    result = hurwicz(matriz, coheficiente) ;
-                break;
-                case 'savage': 
-                    console.log("Inicia savage");
-                    result = savage(matriz) ;
-                break;
-                case 'optimista': 
-                    console.log("Inicia optimista");
-                    result = optimista(matriz) ;
-                break;
-                case 'pesimista': 
-                    console.log("Inicia pesimista");
-                    result = pesimista(matriz) ;
-                break;
-            }
-            $("#result").html("Valor Esperado: " + result);
         }
-    });
 
-    $("#columnas").on('blur', function () {
+        console.log(result);
+        $("#result").html("Valor Esperado: " + result);
+
+    }
+
+    
+});
+
+
+
+
+    $("#columnas").on('blur', function(){
+
         var col = $("#columnas").val();
         var fil = $("#filas").val();
         var obj = "";
 
         // console.log("Longitud: "+col.length);
-        if ((col.length > 0 && fil.length > 0) && (col != 0 && fil != 0)) {
+
+        if((col.length > 0 && fil.length > 0) && (col != 0 && fil != 0))
+        {
             // console.log('OKK');
 
-            obj += "<table>"
-            for (let a = 0; a < col; a++) {
-                obj += "<tr>";
-                for (let b = 0; b < fil; b++) {
-                    obj += "<td>  <input style='width:50px;' type='number' id='" + a + "-" + b + "'>  <td>";
-                }
-                obj += "</tr>";
-            }
-            obj += "</table>";
+                 obj += "<table>"
+     
+             for (let a = 0; a < col; a++) {
+                 obj += "<tr>";
+                 for (let b = 0; b < fil; b++) {
+                     obj += "<td>  <input style='width:50px;' type='number' id='"+a+"-"+b+"'>  <td>";
+                 }
+                 obj += "</tr>";
+             }
+             obj += "</table>";
+             
+             $(".matriz").html(obj);
+             $(".reload").removeClass('hide');
+             $(".reload").attr('id', col+"-"+fil);
 
-            $(".matriz").html(obj);
-            $(".reload").removeClass('hide');
-            $(".reload").attr('id', col + "-" + fil);
-
-        } else if ((col.length == 0 || fil.length == 0) || (col == 0 || fil == 0)) {
+        }else if((col.length == 0 || fil.length == 0)|| (col == 0 || fil == 0)){
             // console.log('KOO');
             // alert("Es necesario indicar columnas");
             // $("#columnas").focus();
             // $("#columna").addClass('is-invalid');
         }
+
+
+
     });
 
-    $(".reload").on('click', function () {
-        var range = $(this).attr('id');
-        var matriz = reloadTable(range);
+    $(".reload").on('click', function(){
+
+            var range = $(this).attr('id');
+            var matriz = reloadTable(range);
+
     });
+
+    
+
+
 });
 
-function getRandomNumber(min, max) {
+function getRandomNumber(min, max) 
+  {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
-}
+  }
 
-function reloadTable(range) {
+  function reloadTable(range)
+ {
     range = range.split('-');
     var col = range[0];
     var fil = range[1];
@@ -89,35 +118,37 @@ function reloadTable(range) {
     // var arrMedian = [];
 
     for (let a = 0; a < col; a++) {
-        var arrMedian = [];
-        for (let b = 0; b < fil; b++) {
-            var number = getRandomNumber(1, 999);
-            arrMedian.push(number);
-            var id = a + "-" + b;
-            $("#" + id).val(number);
-        }
-        arrGrande.push(arrMedian);
+     var arrMedian = [];
+     for (let b = 0; b < fil; b++) {
+         var number = getRandomNumber(1,999);
+         arrMedian.push(number);
+         var id = a+"-"+b;
+         $("#"+id).val(number);
+     }
+      arrGrande.push(arrMedian);
     }
+
     return arrGrande;
-}
+  }
 
-
-function getMatriz() {
+  function getMatriz()
+  {
     var range = $(".reload").attr('id');
-    range = range.split('-');
-    var col = range[0];
-    var fil = range[1];
-    var arrGrande = [];
-    // var arrMedian = [];
-
-    for (let a = 0; a < col; a++) {
-        var arrMedian = [];
-        for (let b = 0; b < fil; b++) {
-            var id = a + "-" + b;
-            var number = $("#" + id).val();
-            arrMedian.push(parseInt(number));
-        }
-        arrGrande.push(arrMedian);
-    }
-    return arrGrande;
-}
+     range = range.split('-');
+     var col = range[0];
+     var fil = range[1];
+     var arrGrande = [];
+     // var arrMedian = [];
+ 
+     for (let a = 0; a < col; a++) {
+      var arrMedian = [];
+      for (let b = 0; b < fil; b++) {
+          var id = a+"-"+b;
+          var number = $("#"+id).val();
+          arrMedian.push(parseInt(number));
+      }
+       arrGrande.push(arrMedian);
+     }
+ 
+     return arrGrande;
+   }
